0908dd990b583433200dd82dfd4b23392f761393
Oleg <infinitu1327@gmail.com>
2022-08-27 14:07:28 +0300

    Add CategoryClient tests & update azure-pipelines

---
+++ Splitwise.Tests/Clients/CategoryClientTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Splitwise.Clients;
+using Xunit;
+using FluentAssertions;
+
+namespace Splitwise.Tests.Clients;
+
+public class CategoryClientTests
+{
+    [Fact]
+    public async Task ListAsync_Works()
+    {
+        var anonymousSplitwiseClient = new AnonymousSplitwiseClient();
+
+        // Act
+
+        var categories = await anonymousSplitwiseClient.Category.ListAsync();
+
+        // Assert
+
+        categories.Should().NotBeNullOrEmpty();
+    }
+}
